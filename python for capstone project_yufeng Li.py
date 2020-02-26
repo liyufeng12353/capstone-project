39ac64dcd80bddadeb4ef249921d71bb4398019b
liyufeng12353 <61488766+liyufeng12353@users.noreply.github.com>
2020-02-26 20:42:27 +0800

    Add files via upload

---
+++ python for capstone project_yufeng Li.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#This notebook will be mainly used for capstone project
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[4]:
+
+
+import pandas as pd
+
+
+# In[6]:
+
+
+print("Hello Capstone Project Course!")
+
